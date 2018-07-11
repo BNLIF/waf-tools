1b81c07eddda202374211fc9d16bb2adf6ce68bd
brookerussell <brooke.russell@yale.edu>
2018-07-11 17:22:19 -0400

    add TMVA

--- find_root.py
+++ find_root.py
@@ -29,7 +29,8 @@ def configure(cfg):
     cfg.find_program('rootcint', var='ROOTCINT', path_list=path_list)
     cfg.find_program('rlibmap', var='RLIBMAP', path_list=path_list, mandatory=False)
 
-    cfg.env.LIB_ROOTSYS += ['Minuit2','TreePlayer','Spectrum']
+    #cfg.env.LIB_ROOTSYS += ['Minuit2','TreePlayer','Spectrum']
+    cfg.env.LIB_ROOTSYS += ['Minuit2','TreePlayer','Spectrum','TMVA']
     return
 
 @conf
